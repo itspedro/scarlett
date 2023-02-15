fe4125c01a643f71402ea709e893d6eef2f6160e
Pedro Reis <pedluhi@outlook.com>
2023-02-15 09:37:10 -0300

    teste

--- src/commands/tools/avatar.js
+++ src/commands/tools/avatar.js
@@ -3,15 +3,14 @@ const { SlashCommandBuilder, Embed, EmbedBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("avatar")
-    .setDescription("Responde o avatar de determinado membro")
-    .addUserOption((option) => option
-      .setName("nome")
-      .setDescription("Nome do úsuario desejado")
-      .setRequired(true)
-    ),
+    .setDescription("Responde o avatar de determinado membro"),
+    // .addUserOption((option) => option
+    //   .setName("nome")
+    //   .setDescription("Nome do úsuario desejado")
+    // ),
   async execute(interaction, client) {
 
-    const member = interaction.options.getUser('nome');
+    const member = interaction.getUser('nome');
 
     const embed = new EmbedBuilder()
         .setTitle(`O avatar de **${member.username}`)
