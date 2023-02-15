cc9e077d023a1e651c5ae241443fe69adf2a3359
Pedro Reis <pedluhi@outlook.com>
2023-02-15 10:56:40 -0300

    ping

--- src/commands/tools/ping.js
+++ src/commands/tools/ping.js
@@ -3,14 +3,14 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new  SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Responde Pong!'),
+        .setDescription('Responde a latencia do bot!'),
     async execute(interaction, client) {
         const message = await interaction.deferReply({
             fetchReply: true
         });
 
-        const newMessage = `Pong`
-        await interaction.editReply({
+        const newMessage = `Websocket heartbeat: ${client.ws.ping}ms.\nRoundtrip latency: ${message.createdTimestamp - interaction.createdTimestamp}ms.`
+        await interaction.reply({
             content: newMessage
         });
     }
