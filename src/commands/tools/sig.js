46519b7f57937fc8fe4c5d655cc44c09b1d6adf0
Pedro Reis <pedluhi@outlook.com>
2023-02-17 18:49:38 -0300

    novos comandos

---
+++ src/commands/tools/sig.js
@@ -0,0 +1,37 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const fetch = require("node-fetch");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("sig")
+        .setDescription("Responde a definição do termo pedido")
+        .addStringOption((option) => 
+            option
+                .setName("termo")
+                .setRequired(true)
+                .setDescription("termo desejado")
+    ),
+    async execute(interaction) {
+        
+        let termo = interaction.options.getString("termo");
+
+        const urlSen = await fetch(`https://dicio-api-ten.vercel.app/v2/frases/${termo}`);
+        const frase = await urlSen.json();
+        
+
+        const urlDef = await fetch(`https://dicio-api-ten.vercel.app/v2/${termo}`);
+        const def = await urlDef.json();
+
+        const embed = new EmbedBuilder()
+            .setTitle(termo)
+            .setColor(0x18e1ee)
+            .setDescription(def[0].partOfSpeech)
+            .addFields(
+                { name: 'Significado ', value: def[0].meanings.toString() },
+                { name:'Frase', value: `"${frase[0].sentence}" **${frase[0].author}**` })
+
+        await interaction.reply({
+            embeds: [embed]
+        });
+    }
+}
