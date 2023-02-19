851da12aa6dad4dcfe25cf62bc55e7bc5b0c2509
Pedro Reis <pedluhi@outlook.com>
2023-02-19 19:05:40 -0300

    bomdia + API changes

--- src/commands/tools/bomdia.js
+++ src/commands/tools/bomdia.js
@@ -2,29 +2,53 @@ const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require("disc
 const cron = require("cron");
 const { API_KEY } = process.env;
 const fetch = require("node-fetch");
+const Guild = require ('../../schemas/guild');
+
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("teste")
-    .setDescription("teste!")
-    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+    .setName("bomdia")
+    .setDescription("Seleciona canal e hora para mandar bom dia")
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+    .addChannelOption((option) =>
+        option
+          .setName("chat")
+          .setDescription("Chat desejado")
+          .setRequired(true)
+    )
+    .addIntegerOption((option) =>
+        option
+          .setName("horas")
+          .setDescription("Horas desejada")
+          .setRequired(true)
+    )
+    .addIntegerOption((option) =>
+        option
+          .setName("minutos")
+          .setDescription("Minutos desejado")
+          .setRequired(true)
+    ),
   
   async execute(interaction) {
+
+      let guildProfile = await Guild.findOne({ guildId: interaction.guild.id });
+      const channelLog = interaction.guild.channels.cache.get(guildProfile.guildLog);
 
       const channel = interaction.options.getChannel("chat");
       const horas = interaction.options.getInteger("horas");
       const minutos = interaction.options.getInteger("minutos");
+
       const zeroPad = (num) => num.toString().padStart(2, '0')
 
       const bomDia = new cron.CronJob({
         cronTime: `00 ${zeroPad(!minutos ? 00 : minutos)} ${zeroPad(horas)} * * *`,
         onTick: async() => {
   
-        const url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=bomdia&limit=25&offset=0&rating=g&lang=pt`;
+        const url = `https://api.tenor.com/v2/search?q=bomdia&key=${API_KEY}`;
         const resposta = await fetch(url);
         const resultado = await resposta.json();
-        const index = Math.floor(Math.random() * resultado.data.length);
-        const gif = `https://media4.giphy.com/media/${resultado.data[index].id}/giphy.gif?&rid=giphy.gif`
+        const index = Math.floor(Math.random() * resultado.results.length);
+        const gif = resultado.results[index].media_formats.gif.url
   
         const embed = new EmbedBuilder()
           .setTitle(`Bom Dia!`)
@@ -43,6 +67,14 @@ module.exports = {
     console.log("Bom dia");
 
     const response = `>>> O bom dia foi setado para o chat: ${channel}\nTodos os dias às: **${zeroPad(horas)} Hrs e ${zeroPad(!minutos ? 00 : minutos)} Min.**`
+
+    const embedLog = new EmbedBuilder()
+    .setTitle(`SET`)
+    .setColor(0x00FF00)
+    .setDescription(`O bom dia foi setado para o chat: ${channel}\nTodos os dias às: **${zeroPad(horas)} Hrs e ${zeroPad(!minutos ? 00 : minutos)} Min.**`)
+    .setTimestamp();
+
+    channelLog.send({ embeds:[embedLog] });
 
     await interaction.reply({
       content: response,
