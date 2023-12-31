ff98e94bef0f198125e17657d4bcb5fcbbe10f60
Pedro Reis <itspdro@outlook.com>
2023-12-31 11:07:09 -0300

    [~]chore: update chart.

--- src/commands/tools/moeda.js
+++ src/commands/tools/moeda.js
@@ -16,8 +16,7 @@ module.exports = {
           { name: "BRL", value: "BRL" },
           { name: "ARS", value: "ARS" },
           { name: "USD", value: "USD" },
-          { name: "TRY", value: "TRY" },
-          { name: "BTC", value: "BTC" }
+          { name: "TRY", value: "TRY" }
         )
     )
     .addStringOption((option) =>
@@ -29,8 +28,7 @@ module.exports = {
           { name: "BRL", value: "BRL" },
           { name: "ARS", value: "ARS" },
           { name: "USD", value: "USD" },
-          { name: "TRY", value: "TRY" },
-          { name: "BTC", value: "BTC" }
+          { name: "TRY", value: "TRY" }
         )
     ),
   async execute(interaction) {
@@ -41,24 +39,34 @@ module.exports = {
     const url = `https://economia.awesomeapi.com.br/last/${moeda1}-${moeda2}`;
     const resposta = await fetch(url);
     const resultado = await resposta.json();
+    
+    if (!resultado[`${moeda1}${moeda2}`]) {
+      return interaction.reply({
+        content: `Moeda inválida.`,
+        ephemeral: true,
+      });
+    }
+    
     const valor = parseFloat(resultado[`${moeda1}${moeda2}`].bid).toFixed(2);
 
     const variacao = parseFloat(resultado[`${moeda1}${moeda2}`].varBid).toFixed(2);
     const variacaoPor = parseFloat(resultado[`${moeda1}${moeda2}`].pctChange).toFixed(2);
 
-    const channel = interaction.channel;
-
-    // make a chart getting last 15 days    
-
     const canvas = createCanvas(800, 500);
     const context = canvas.getContext('2d');
-    context.fillStyle = "black";
+    
+    context.fillStyle = "#2b2d31";
     context.fillRect(0, 0, canvas.width, canvas.height);
 
     context.fillStyle = "white";
     context.font = "bold 32px Arial";
     context.textBaseline = "hanging";
-    context.fillText(`Últimos 30 dias`, 15, 15);
+    context.fillText(`${resultado[`${moeda1}${moeda2}`].name}`, 15, 15);
+
+    context.fillStyle = "white";
+    context.font = "normal 24px Arial";
+    context.textBaseline = "hanging";
+    context.fillText(`Últimos 30 dias`, 20, 55);
 
     context.fillStyle = "#808080";
     context.fillRect(10,100, canvas.width -20, 5)
@@ -66,13 +74,16 @@ module.exports = {
     context.fillStyle = "white";
     context.font = "bold 32px Arial";
     context.textBaseline = "hanging";
-    context.fillText(`${valor}`, 20, 110);
-    const valorWidth = context.measureText(`${valor}`).width;
+    context.fillText(`${valor} ${moeda2}`, 20, 110);
+    const valorWidth = context.measureText(`${valor} ${moeda2}`).width;
 
-    context.fillStyle = "green";
-    context.font = "bold 32px Arial";
-    context.textBaseline = "hanging";
-    context.fillText(`${variacao}(${variacaoPor}%)`, 45 + valorWidth, 110);
+    if (variacao > 0 || variacao < 0) {
+      context.fillStyle = "white";
+      context.font = "bold 32px Arial";
+      context.textBaseline = "hanging";
+      context.fillText(`${variacao > 0 ? `+${variacao}` : `${variacao}` } | (${variacaoPor > 0 ? `+${variacaoPor}`: `${variacaoPor}`}%)`,
+       50 + valorWidth, 110);
+    }
 
     const urlGrafico = `https://economia.awesomeapi.com.br/json/daily/${moeda1}-${moeda2}/30`;
     const res = await fetch(urlGrafico);
@@ -94,20 +105,29 @@ module.exports = {
       context.strokeStyle = "white";
       context.stroke();
 
+      let gradient = context.createLinearGradient(i, canvas.height - 100 - previousHeight, i, canvas.height - 100 + curHeight);
+      gradient.addColorStop(0, "rgba(255,255,255,0)");
+      gradient.addColorStop(1, "rgba(255,255,255,0.5)");
+      context.fillStyle = gradient;
+      context.fillRect(10 + i * 25, canvas.height - 100 - previousHeight, 25, canvas.height );
+
+
       previousHeight = curHeight;
     };
 
-
-
     const embed = new EmbedBuilder()
-      .setTitle(`Cotação`)
-      .setDescription(`**${moeda1}** para **${moeda2}**`)
+      .setTitle(`Cotação de **${moeda1}** para **${moeda2}**`)
       .setColor(0x00FF00)
       .setImage("attachment://chart.png")
       .addFields([
         {
-          name: `Cotação atual`,
+          name: `Valor atual`,
           value: `**${valor}** ${moeda2}`,
+          inline: true,
+        },
+        {
+          name: `Última atualização`,
+          value: resultado[`${moeda1}${moeda2}`].create_date,
           inline: true,
         }
       ]);
